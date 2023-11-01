38007cef64bc25dd715ef51fca60363e966eb6c3
unknown <surafel@ienetworks.co>
2023-11-01 21:44:21 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Input } from "antd";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <Input className="p-5" placeholder="Basic usage" />;
+}
+
+export default App;
